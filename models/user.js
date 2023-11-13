f9c213200b422d6e53c5d13fa41475da042810b7
Santiago Orozco Buri <santiago.orozco@genoneclt.org>
2023-11-13 16:53:21 -0500

    hashed passwords

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
     firstName: {type: String, required: [true, "First name field cannot be empty"]},
@@ -7,3 +8,23 @@ const userSchema = new Schema({
     email: {type: String, required: [true, "Email field cannot be empty"], unique: true},
     password: {type: String, required: [true, "Password field cannot be empty"]}
 });
+
+userSchema.pre('save', function(next) {
+    let user = this;
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.hash(user.password, 10)
+        .then(hash => {
+            user.password = hash;
+            next();
+        })
+        .catch(err => next(err));
+});
+
+userSchema.methods.comparePassword = function(inputPassword) {
+    let user = this;
+    return bcrypt.compare(inputPassword, user.password);
+}
+
+module.exports = mongoose.model("User", userSchema);
